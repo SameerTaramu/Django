d6ec5878d65672e6b9ae4e00801d2f46ed102f8b
Sameer <taramusameer522@gmail.com>
2026-05-14 22:06:41 +0545

    first commit

---
+++ students/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse 
+
+class Student(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    age = models.IntegerField()
+
+def __str__(self):
+    return self.name
+
+def get_absolute_url(self):
+    return reverse('student_details', kwarg={'pk': self.pk})
+
+# Create your models here.
