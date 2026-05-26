425ba24a7e9e12ae6e5e6bf67524e0e0f9d77376
Sameer <taramusameer522@gmail.com>
2026-05-26 22:00:36 +0545

    allowed all host

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^q49y0%qa844y2q=h37b5!+nmtj9#=k&3#%5k(^zmq+kz&uou#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
